11e0a20885c20967e443bfdbf28adbc71922672e
mpndev <martin.nikolov@ffwagency.com>
2022-01-07 00:06:19 +0200

    fix typos

--- resources/js/pages/login/create.vue
+++ resources/js/pages/login/create.vue
@@ -24,7 +24,7 @@
             <span class="ml-1"><b>{{ $t('or') }}</b></span>
             <button class="button is-dark ml-1 mr-1" @click="loginWithGithub">{{ $t('Login with github') }}<span class="fab fa-github ml-2"></span></button>
             <span class="ml-1 mr-1"><b>, {{ $t('or') }}</b></span>
-            <button class="button is-danger ml-1 mr-1" @click="loginWithGoogle">{{ $t('Login with gogle') }}<span class="fab fa-google ml-2"></span></button>
+            <button class="button is-danger ml-1 mr-1" @click="loginWithGoogle">{{ $t('Login with google') }}<span class="fab fa-google ml-2"></span></button>
         </div>
     </div>
 </template>
